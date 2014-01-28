cb808057f26562bec2e10975cbe7950a3a6bb6b0
Florian Rossiaud <florian.rossiaud@gmail.com>
2014-01-28 14:35:38 +0100

    Add of the sample

--- test/hydraters/hydraters.js
+++ test/hydraters/hydraters.js
@@ -5,28 +5,31 @@ require('should');
 
 var hydraters = require('./list.js');
 
-
-describe("Ping all the hydraters on /hydrate.", function () {
-  hydraters.list.forEach(function(name) {
-    console.log(hydraters.params[name].url);
-    var req = request(hydraters.params[name].url);
-    it("should receive a code 405 for the " + name, function (done) {
-      req.post('/hydrate')
-        .expect(405)
-        .end(done);
+describe("Test on the hydrators", function () {
+  describe("Ping all the hydraters on /hydrate.", function () {
+    hydraters.list.forEach(function(name) {
+      console.log(hydraters.params[name].url);
+      var req = request(hydraters.params[name].url);
+      it("should receive a code 405 for the " + name, function (done) {
+        req.post('/hydrate')
+          .expect(405)
+          .end(done);
+      });
     });
   });
-});
 
 
-describe("Send a document to all the hydraters on /hydrate and test it", function () {
-  hydraters.list.forEach(function(name) {
-    var req = request(hydraters.params[name].url);
-    it.skip("should receive a json code " + name, function (done) {
-      req.post('/')
-        .send(hydraters.params[name].post)
-        .expect(405)
-        .end(done);
+  describe("Send a document to all the hydraters on /hydrate and test the answer", function () {
+    hydraters.list.forEach(function(name) {
+      var req = request(hydraters.params[name].url);
+      it.skip("should receive a json code " + name, function (done) {
+        req.post('/')
+          .send(hydraters.params[name].post)
+          .expect(200)
+          .end(function(err, res) {
+            done();
+          });
+      });
     });
   });
 });
