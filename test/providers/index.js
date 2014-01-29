03029a1700a6489bbd7f7d05bed2f90870bde1c9
Neamar <neamar@neamar.fr>
2014-01-29 18:14:43 +0100

    Providers test

--- test/providers/index.js
+++ test/providers/index.js
@@ -1,17 +1,23 @@
-var providers = [
-  'http://dropbox.provider.anyfetch.com'  
-  'http://gmail.provider.anyfetch.com'  
-  'http://gcontacts.provider.anyfetch.com'  
-]
+'use strict';
 
-describe("Test hydraters", function() {
+require('should');
+var request = require('supertest');
+
+var providers = [
+  'http://dropbox.provider.anyfetch.com',
+  'http://gmail.provider.anyfetch.com',
+  'http://gcontacts.provider.anyfetch.com',
+];
+
+describe("Test providers", function() {
   describe("are up", function() {
-    Object.keys(hydraters).forEach(function(url) {
+    providers.forEach(function(url) {
       it("`" + url + "` should be up", function(done) {
         request(url)
-          .get('/status')
-          .expect(200)
+          .get('/update')
+          .expect(405)
           .end(done);
       });
     });
   });
+});
