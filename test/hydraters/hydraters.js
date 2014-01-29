0e3b2477101dba435b186451cb0309aad335569e
Neamar <neamar@neamar.fr>
2014-01-29 15:45:21 +0100

    Refactor config

--- test/hydraters/hydraters.js
+++ test/hydraters/hydraters.js
@@ -3,11 +3,104 @@
 require('should');
 var request = require('supertest');
 
-describe("Test hydraters", function() {
-  var hydraters = ['http://plaintext.hydrater.anyfetch.com', 'http://pdf.hydrater.anyfetch.com', 'http://office.hydrater.anyfetch.com', 'http://image.hydrater.anyfetch.com', 'http://ocr.hydrater.anyfetch.com', 'http://eml.hydrater.anyfetch.com'];
 
+// Build a checker-function to compare a reply with a file
+var generateCompareFunction = function(file) {
+  return function(res, done) {
+    require(file).should.eql(res);
+
+    done();
+  };
+};
+
+var hydraters = {
+  'http://plaintext.hydrater.anyfetch.com': {
+    payload: {
+      file_path: "https://raw2.github.com/Papiel/anyfetch-test/cb808057f26562bec2e10975cbe7950a3a6bb6b0/test/hydraters/samples/plaintext.hydrater.anyfetch.com.test.doc",
+      long_poll: 1,
+      document: {
+        document_type: "file",
+        metadatas: {},
+        datas: {},
+        identifier: 'plaintext-test'
+      }
+    },
+    expected: generateCompareFunction('./samples/plaintext.hydrater.anyfetch.com.expected.json')
+  },
+  'http://pdf.hydrater.anyfetch.com': {
+    payload: {
+      file_path: "https://raw2.github.com/Papiel/anyfetch-test/cb808057f26562bec2e10975cbe7950a3a6bb6b0/test/hydraters/samples/pdf.hydrater.anyfetch.com.test.pdf",
+      long_poll: 1,
+      document: {
+        document_type: 'document',
+        metadatas: {
+          path: 'pdf.hydrater.anyfetch.com.test.pdf',
+          mime_type: 'application/pdf'
+        },
+        datas: {},
+        identifier: 'pdf-test'
+      }
+    }
+  },
+  'http://office.hydrater.anyfetch.com': {
+    payload: {
+      access_token: "123",
+      file_path: "https://raw2.github.com/Papiel/anyfetch-test/cb808057f26562bec2e10975cbe7950a3a6bb6b0/test/hydraters/samples/office.hydrater.anyfetch.com.test.doc",
+      long_poll: 1,
+      document: {
+        document_type: 'document',
+        metadatas: {
+          path: 'office.hydrater.anyfetch.com.test.doc'
+        },
+        datas: {},
+        identifier: 'office-test'
+      }
+    }
+  },
+  'http://image.hydrater.anyfetch.com': {
+    payload: {
+      file_path: "https://raw2.github.com/Papiel/anyfetch-test/cb808057f26562bec2e10975cbe7950a3a6bb6b0/test/hydraters/samples/image.hydrater.anyfetch.com.test.png",
+      long_poll: 1,
+      example: {
+        document_type: 'file',
+        metadatas: {},
+        datas: {},
+        identifier: 'image-test'
+      }
+    }
+  },
+  'http://ocr.hydrater.anyfetch.com': {
+    payload: {
+      file_path: "https://raw2.github.com/Papiel/anyfetch-test/cb808057f26562bec2e10975cbe7950a3a6bb6b0/test/hydraters/samples/ocr.hydrater.anyfetch.com.test.png",
+      long_poll: 1,
+      document: {
+        document_type: 'image',
+        metadatas: {},
+        datas: {},
+        identifier: 'ocr-test'
+      }
+    }
+  },
+  'http://eml.hydrater.anyfetch.com': {
+    payload: {
+      access_token: "321",
+      file_path: "https://raw2.github.com/Papiel/anyfetch-test/cb808057f26562bec2e10975cbe7950a3a6bb6b0/test/hydraters/samples/eml.hydrater.anyfetch.com.test.eml",
+      long_poll: 1,
+      document: {
+        document_type: 'document',
+        metadatas: {
+          path: '/file.eml'
+        },
+        datas: {},
+        identifier: 'eml-test'
+      }
+    }
+  },
+};
+
+describe("Test hydraters", function() {
   describe("are up", function() {
-    hydraters.forEach(function(url) {
+    Object.keys(hydraters).forEach(function(url) {
       it("`" + url + "` should be up", function(done) {
         request(url)
           .get('/status')
@@ -17,6 +110,25 @@ describe("Test hydraters", function() {
     });
   });
 
-  describe.skip("are working", function() {
+  describe("are working", function() {
+    Object.keys(hydraters).forEach(function(url) {
+      if(!hydraters[url].expected) {
+        return;
+      }
+
+      it("`" + url + "` should hydrate file", function(done) {
+        request(url)
+          .post('/hydrate')
+          .send(hydraters[url].payload)
+          .expect(200)
+          .end(function(err, res) {
+            if(err) {
+              throw err;
+            }
+
+            hydraters[url].expected(res.body, done);
+          });
+      });
+    });
   });
 });
