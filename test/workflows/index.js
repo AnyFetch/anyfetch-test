1dc3e5087d0b6c74a8cbe5480812cfd61ee9de6f
Neamar <neamar@neamar.fr>
2014-02-04 11:48:35 +0100

    Simplest workflow

---
+++ test/workflows/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+require('should');
+
+var helpers = require('./helpers.js');
+
+describe.only("Test workflow", function() {
+  before(helpers.createAccount);
+  before(helpers.resetAccount);
+
+  it("should be able to create an account", function(done) {
+    // We should be able to login using supplied credentials
+    helpers.apiRequest('get', '/')
+      .expect(200)
+      .expect(helpers.expectJSON('name', 'test@anyfetch.com'))
+      .end(done);
+  });
+  after(helpers.deleteAccount);
+});
