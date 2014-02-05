1014fed6b565b62e84cc4529e99368a7b4d5ae2a
Neamar <neamar@neamar.fr>
2014-02-05 18:07:16 +0100

    Search and retrieve workflow

--- test/workflows/providers.js
+++ test/workflows/providers.js
@@ -6,14 +6,15 @@ var helpers = require('./helpers.js');
 
 describe("Test providers workflow", function() {
   before(helpers.createAccount);
+  before(helpers.resetAccount);
   before(helpers.getToken);
 
   var payload = {
     identifier:'test-identifier',
     metadatas: {
-      path:'hello world'
+      text:'hello world'
     },
-    document_type: 'file',
+    document_type: null,
     user_access: null
   };
 
@@ -24,19 +25,40 @@ describe("Test providers workflow", function() {
       .expect(helpers.expectJSON('identifier', payload.identifier))
       .expect(200)
       .end(function(err, res) {
+        if(err) {
+          throw err;
+        }
         payload.id = res.body.id;
-        done(err);
+
+        done();
       });
   });
 
-  it("should be able to get a document", function(done) {
-    // We should be able to get the document, and it should be the correct doc
-    helpers.basicApiRequest('get', '/documents?search=hello')
-      .expect(function(res){
-        res.body.datas[0].should.have.property('id', payload.id);
+  it("should be able to retrieve a document", function(done) {
+    // We should be able to get the document after providing
+    helpers.basicApiRequest('get', '/documents/' + payload.id)
+      .expect(200)
+      .expect(function(res) {
+        res.body.should.have.property('id', payload.id);
+        res.body.should.have.property('datas');
       })
       .expect(200)
       .end(done);
+  });
+
+  it("should be able to search for a document", function(done) {
+    // Wait for ES indexing before keeping on
+    setTimeout(function() {
+      // We should be able to get the document via ES
+      helpers.basicApiRequest('get', '/documents?search=hello')
+        .expect(200)
+        .expect(function(res) {
+          res.body.should.have.property('datas').with.lengthOf(1);
+          res.body.datas[0].should.have.property('id', payload.id);
+        })
+        .expect(200)
+        .end(done);
+    }, 1000);
   });
 
   after(helpers.deleteAccount);
