8d98aebd9721093043b0a1da06f4085d530aa9a3
Amoki <hugoduroux@gmail.com>
2014-03-05 14:11:13 +0100

    add test staging front

--- test/frontend/index.js
+++ test/frontend/index.js
@@ -4,10 +4,18 @@ require('should');
 var request = require('supertest');
 
 var url = 'http://anyfetch.com';
+var staging_url = 'http://anyfetch.com';
 
 describe("Test frontend", function() {
   it("`" + url + "` should be up", function(done) {
     request(url)
+      .get('/')
+      .expect(200)
+      .end(done);
+  });
+
+  it("`" + staging_url + "` should be up", function(done) {
+    request(staging_url)
       .get('/')
       .expect(200)
       .end(done);
