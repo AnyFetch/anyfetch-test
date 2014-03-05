a61b86315d8a9bc8349213ee7cb082d46c069b67
Amoki <hugoduroux@gmail.com>
2014-03-05 14:54:23 +0100

    update back/front test

--- test/frontend/index.js
+++ test/frontend/index.js
@@ -3,21 +3,24 @@
 require('should');
 var request = require('supertest');
 
-var url = 'http://anyfetch.com';
-var staging_url = 'http://anyfetch.com';
+var urlFront = ['http://anyfetch.com', 'http://staging.anyfetch.com'];
+var urlBack = ['http://api.anyfetch.com', 'http://staging.api.anyfetch.com'];
 
-describe("Test frontend", function() {
-  it("`" + url + "` should be up", function(done) {
-    request(url)
+describe("Test front and back ends", function() {
+  urlFront.forEach(function(url){
+    it("`" + url + "` should be up", function(done) {
+      request(url)
       .get('/')
       .expect(200)
       .end(done);
+    });
   });
-
-  it("`" + staging_url + "` should be up", function(done) {
-    request(staging_url)
-      .get('/')
+  urlBack.forEach(function(url){
+    it("`" + url + "` should be up", function(done) {
+      request(url)
+      .get('/status')
       .expect(200)
       .end(done);
+    });
   });
 });
