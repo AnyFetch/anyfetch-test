083f4c52317f8cc6dde47c2822d483f2bf0108bd
Amoki <hugoduroux@gmail.com>
2014-05-19 17:17:46 +0200

    ready to https

--- test/config.js
+++ test/config.js
@@ -2,6 +2,6 @@
 
 // Exports configuration
 module.exports = {
-  apiUrl: process.env.API_URL || "http://api.anyfetch.com",
+  apiUrl: process.env.API_URL || "https://api.anyfetch.com",
   basicCredential: process.env.CREDENTIALS || "dGVzdEBhbnlmZXRjaC5jb206cGFzc3dvcmQ=",
 };
