3bc376f50f4f775514ddf646d4ca247874ee3c87
Amoki <hugoduroux@gmail.com>
2014-07-03 14:53:19 +0200

    some optimisation

--- test/env.js
+++ test/env.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var env = process.env.NODE_ENV;
+console.log("Testing with", env || "test-production", "environment");
 
-console.log("Testing with", env ? env: "../test-production.json", "environment");
-
-module.exports = env ? require('../' + env + '.json') : require('../test-production.json');
+module.exports = env ? require('../' + env + '.json') : require('../production.json');
+module.exports.credentials = process.env.CREDENIALS || "dGVzdEBhbnlmZXRjaC5jb206cGFzc3dvcmQ=";
