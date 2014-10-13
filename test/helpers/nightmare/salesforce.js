4c7cac70586c5f920bceb0c9336bafa7ecfcb3b9
buathi_q <buathi_q@epitech.eu>
2014-10-13 15:45:16 +0200

    Add salesforce nightmare helper

---
+++ test/helpers/nightmare/salesforce.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var env = require('../../../config');
+
+module.exports.login = function login(email, password) {
+  return function(nightmare) {
+   nightmare
+    .wait('#login')
+    .type('#username', email)
+    .type('#password', password)
+    .click('#login')
+    .wait();
+  };
+};
+
+module.exports.authorize = function authorize() {
+  return function(nightmare) {
+    nightmare
+      .wait('#oaapprove')
+      .click('#oaapprove')
+      .wait();
+  };
+};
