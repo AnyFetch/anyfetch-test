0d065c7197a7ac00d3ca1d46236c5f8d052e7b9b
Amoki <hugoduroux@gmail.com>
2014-10-27 10:49:44 +0100

    status into circle.sh

--- circle.sh
+++ circle.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 case $CIRCLE_NODE_INDEX in
-  0) export API_ENV=staging; LONG=1 npm test ;;
-  1) export API_ENV=production; LONG=1 npm test ;;
+  0)  export API_ENV=staging
+      node bin/status
+      export LONG=1
+      npm test
+      ;;
+  1)  export API_ENV=production
+      node bin/status
+      export LONG=1
+      npm test
+      ;;
 esac
 
