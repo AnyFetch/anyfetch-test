36d1a5073c7e6c1e6c21f429e7fd042a926c897f
Amoki <hugoduroux@gmail.com>
2014-10-27 15:38:05 +0100

    hope

--- test/test/api/workflows/dependencies.js
+++ test/test/api/workflows/dependencies.js
@@ -64,7 +64,7 @@ describe("Test hydraters dependencies", function() {
       hydratedDocument.metadata.should.have.property('description', '© 2010 Frédéric Ruaudel, All Rights Reserved');
       hydratedDocument.metadata.should.have.property('keywords', '500px, Adulte, Blog FR, Fotografar2014, Homme, Personne, Xavier Bernard, iPhoto');
       hydratedDocument.data.should.have.property('display');
-      hydratedDocument.data.should.have.property('thumb');
+      hydratedDocument.metadata.should.have.property('thumb');
       hydratedDocument.should.have.property('hydrated_by');
       hydratersToWait.forEach(function(hydraterToWait) {
         hydratedDocument.hydrated_by.should.containEql(hydraterToWait + "/hydrate");
